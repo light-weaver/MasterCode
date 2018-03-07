26cab7ba4d279c8282d0573872dc4a7fecb69de1
Bhupinder <bhupinder.gmail2.71@gmail.com>
2018-03-07 18:00:38 +0530

    Added graphing Tools

---
+++ Tools/Plot_3d.m
@@ -0,0 +1,10 @@
+%Copy Pareto file to this directory
+%Add legends, axes and title manually
+%Save manually
+
+data = importdata('filename.mat'); 
+objectives = [1,3,5]; %Only 3 objectives
+%=======================================
+FVal = data.FuntionValue(:,objectives);
+close all
+scatter3(FVal(:,1),FVal(:,2),FVal(:,3),'r');
---
+++ Tools/Plot_4d.m
@@ -0,0 +1,14 @@
+%Copy Pareto file to this directory
+%Add legends, axes and title manually
+%Save manually
+
+data = importdata('filename.mat'); 
+objectives = [1,3,5]; %Only 3 objectives
+mrkSize = 50; %defines markersize
+%=======================================
+FVal = data.FuntionValue(:,objectives);
+%FVal = abs(FVal); %Use if you want to use absolute values of FVal
+close all
+colormap jet;
+scatter3(FVal(:,1),FVal(:,2),FVal(:,3),mrkSize,FVal(:,4))
+colorbar
